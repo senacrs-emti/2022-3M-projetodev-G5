74d2dcf2473269bc2c5b6120eb30ac1893b3c71c
BiancaFiRodrigues <biancafigueired.rodrigues@gmail.com>
2022-10-21 11:33:11 -0300

    aa
    aa

---
+++ xadrez/src/visao/JTabuleiro.java
@@ -0,0 +1,19 @@
+package visao;
+
+import javax.swing.JPanel;
+import modelo.tabuleiro;
+
+
+public class JTabuleiro extends JPanel{
+
+    private tabuleiro tabuleiro;
+
+
+    public JTabuleiro(tabuleiro tabuleiro){
+        this.tabuleiro = tabuleiro;
+    }
+
+
+    
+    
+}
